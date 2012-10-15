022f9acb81ec064300fa9b447c682880979405ad
EC2 Default User <ec2-user@ip-10-29-147-60.ec2.internal>
2012-10-15 18:16:08 +0000

    succeeded in multi-user + send buffer-size adjustments. multi-user also closes properly.

--- server.py
+++ server.py
@@ -8,59 +8,48 @@ class StreamHandler(ftpserver.FTPHandler):
         self._close_connection = False
         self._wait_time = wait_time
         self._packet_size = 100
+        self.dtp_handler = DTPPacketHandler
+        self.dtp_handler.set_buffer_size(self._packet_size)
 
     def ftp_RETR(self, file):
         """
         Copied and pasted the code from ftp_RETR here, because we need to set
         the offset of the file reading, and not just handle the ftp_REST case.
         """
-        offset_pos = self._restart_position
+        rest_pos = self._restart_position
         self._restart_position = 0
-        while offset_pos < self.fs.getsize(file):
+        try:
+            fd = self.run_as_current_user(self.fs.open, file, 'rb')
+        except (EnvironmentError, FilesystemError):
+            err = sys.exc_info()[1]
+            why = _strerror(err)
+            self.respond('550 %s.' % why)
+            return
+
+
+        if rest_pos:
+            # Make sure that the requested offset is valid (within the
+            # size of the file being resumed).
+            # According to RFC-1123 a 554 reply may result in case that
+            # the existing file cannot be repositioned as specified in
+            # the REST.
+            ok = 0
             try:
-                try:
-                    fd = self.run_as_current_user(self.fs.open, file, 'rb')
-                except (EnvironmentError, ftpserver.FilesystemError):
-                    err = sys.exc_info()[1]
-                    why = _strerror(err)
-                    self.respond('550 %s.' % why)
-                    return
-
-                if offset_pos:
-                    # Make sure that the requested offset is valid (within the
-                    # size of the file being resumed).
-                    # According to RFC-1123 a 554 reply may result in case that
-                    # the existing file cannot be repositioned as specified in
-                    # the REST.
-                    ok = 0
-                    try:
-                        if offset_pos > self.fs.getsize(file):
-                            raise ValueError
-                        fd.seek(offset_pos)
-                        ok = 1
-                    except ValueError:
-                        why = "Invalid REST parameter"
-                    except (EnvironmentError, FilesystemError):
-                        err = sys.exc_info()[1]
-                        why = _strerror(err)
-                    if not ok:
-                        fd.close()
-                        self.respond('554 %s' % why)
-                        return
-                producer = FilePacketProducer(fd, self._current_type, self._packet_size)
-                data = fd.read(self._packet_size)
-                self.push_dtp_data(producer, isproducer=True, file=fd, cmd="RETR")
-
-            except IOError, err:
-                # initial file did not exist. Otherwise assume things
-                # are broken into chunks...
-                if streaming_count == 1:
-                    why = ftpserver._strerror(err)
-                    self.respond('550 %s.' % why)
+                if rest_pos > self.fs.getsize(file):
+                    raise ValueError
+                fd.seek(rest_pos)
+                ok = 1
+            except ValueError:
+                why = "Invalid REST parameter"
+            except (EnvironmentError, FilesystemError):
+                err = sys.exc_info()[1]
+                why = ftpserver._strerror(err)
+            if not ok:
+                fd.close()
+                self.respond('554 %s' % why)
                 return
-
-            offset_pos += self._packet_size
-        self._close_connection = True
+        producer = FilePacketProducer(fd, self._current_type)
+        self.push_dtp_data(producer, isproducer=True, file=fd, cmd="RETR")        
 
     def ftp_LIST(self, path):
         """Return a list of files in the specified directory to the
@@ -73,11 +62,25 @@ class StreamHandler(ftpserver.FTPHandler):
         try:
             iterator = self.run_as_current_user(self.fs.get_list_dir, movies_path)
         except OSError, err:
-            why = _strerror(err)
+            why = ftpserver._strerror(err)
             self.respond('550 %s.' % why)
         else:
             producer = MovieLister(iterator)
             self.push_dtp_data(producer, isproducer=True, cmd="LIST")
+
+    def _on_dtp_connection(self):
+        """For debugging purposes."""
+        print "Calling _on_dtp_connection."
+        return super(StreamHandler, self)._on_dtp_connection()
+
+class DTPPacketHandler(ftpserver.DTPHandler):
+    def __init__(self, sock_obj, cmd_channel, out_buffer_size=65536):
+        super(DTPPacketHandler, self).__init__(sock_obj, cmd_channel)
+        DTPPacketHandler.ac_out_buffer_size = out_buffer_size
+
+    @staticmethod
+    def set_buffer_size(out_buffer_size):
+        DTPPacketHandler.ac_out_buffer_size = out_buffer_size
 
 class FilePacketProducer(ftpserver.FileProducer):
     """Wraps around FileProducer such that reading is limited by
@@ -85,13 +88,21 @@ class FilePacketProducer(ftpserver.FileProducer):
     Default buffer_size is 65536 as specified in FileProducer.
     """
     def __init__(self, file, type, packet_size=65536, wait_time=1):
-        self.buffer_size = packet_size
         self.wait_time = wait_time
         super(FilePacketProducer, self).__init__(file, type)
+        FilePacketProducer.buffer_size = 131072 #packet_size
+
+    @staticmethod
+    def set_buffer_size(buffer_size):
+        FilePacketProducer.buffer_size = buffer_size
 
     def more(self):
-        return super(FilePacketProducer, self).more()
         time.sleep(self.wait_time)
+        data = super(FilePacketProducer, self).more()
+        # outputStr = "Size of packet to send: %d\n" % sys.getsizeof(data)
+        # sys.stdout.write(outputStr)
+        # sys.stdout.flush()
+        return data
 
 class MovieLister(ftpserver.BufferedIteratorProducer):
     def __init__(self, iterator):
