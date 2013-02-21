b3a6bccbc9308760afd12bc08ee9d96da531433f
Kangwook Lee <kw1jjang@gmail.com>
2013-02-20 19:15:12 -0800

    Revert "new shell scripts"
    This reverts commit 14633681557113753f67ac56f787b7197398c7f6.

--- populate_cache.sh
+++ populate_cache.sh
@@ -1,16 +1,10 @@
 num_of_caches=2
-base_port=49152 # this is the first of the private ports, so we won't disrupt
-                # operations using reserved ports.
+base_port=21
 for (( i = 0; i < num_of_caches; i++ ))
 do
     port_num=$((i+base_port))
     echo "Initiating cache with port # $port_num ..."
-    if [ ! -d "cache"$i ]; then
-        mkdir "cache"$i
-    fi
-    cd "cache"$1
-    sudo python ../cache.py 300000 $port_num & > log.txt
-    cd ..
+    sudo python cache.py 300000 $port_num &
     sleep 5
 done
 
