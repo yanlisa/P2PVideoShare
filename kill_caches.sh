427b95bf57c5f2fce03e595f0a411c2129c6f59d
Kangwook Lee <kw1jjang@gmail.com>
2013-03-11 22:40:43 -0700

    csv changes

--- kill_caches.sh
+++
@@ -1,9 +0,0 @@
-pids=`ps -C python -o pid,cmd | grep "python ../../cache.py" | awk '{print $1}'`
-#processes=`ps -C python -o pid,cmd | grep "python cache.py"`
-# might use the processes var in order to print statements regarding who
-# is being killed
-for pid in $pids
-do
-    echo Killing cache with PID $pid ...
-    sudo kill $pid
-done
--- kill_users.sh
+++
@@ -1,9 +0,0 @@
-pids=`ps -a | grep "python ../../user.py" | awk '{print $1}'`
-#processes=`ps -C python -o pid,cmd | grep "python cache.py"`
-# might use the processes var in order to print statements regarding who
-# is being killed
-for pid in $pids
-do
-    echo Killing user with PID $pid ...
-    sudo kill $pid
-done
