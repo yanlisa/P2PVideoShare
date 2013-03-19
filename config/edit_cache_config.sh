f1015f569f45f4882ad721839613a9870e179a4f
EC2 Default User <ec2-user@ip-10-29-147-60.ec2.internal>
2013-03-19 23:34:48 +0000

    Can modify cache_config.csv using script in config folder.

---
+++ config/edit_cache_config.sh
@@ -0,0 +1,15 @@
+numcaches=5
+if [ ! -z "$1" ] ; then
+    numcaches=$1
+fi
+echo "numcaches: $numcaches"
+print "Removing old cache_config.csv."
+rm cache_config.csv
+
+iplocal=`./ip_local.sh`
+ippublic=`./ip_public.sh`
+for (( i=1; i <= $numcaches; i++ ))
+do
+    printf -v j "%03d" $i
+    echo "$j ${iplocal} 60$j ${ippublic} 50000 30" >> cache_config.csv
+done
