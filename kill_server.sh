fbe53b52830720ca21966c3f4c6b7afe666c0a2a
Lisa-EC2 <lisasyan@gmail.com>
2013-03-20 00:12:09 +0000

    Kill scripts also work on EC2 now.

--- kill_server.sh
+++ kill_server.sh
@@ -1,4 +1,4 @@
-pids=`ps -a | grep "python ../server.py" | awk '{print $1}'`
+pids=`ps -af | grep "python ../server.py" | awk '{print $2}'`
 #processes=`ps -C python -o pid,cmd | grep "python cache.py"`
 # might use the processes var in order to print statements regarding who
 # is being killed
@@ -7,7 +7,7 @@ do
     kill $pid
 done
 
-pids=`ps -a | grep "python ../server_for_cache.py" | awk '{print $1}'`
+pids=`ps -af | grep "python ../server_for_cache.py" | awk '{print $2}'`
 #processes=`ps -C python -o pid,cmd | grep "python cache.py"`
 # might use the processes var in order to print statements regarding who
 # is being killed
--- kill_users.sh
+++ kill_users.sh
@@ -1,4 +1,4 @@
-pids=`ps -a | grep "python ../../user.py" | awk '{print $1}'`
+pids=`ps -af | grep "python ../../user.py" | awk '{print $2}'`
 #processes=`ps -C python -o pid,cmd | grep "python cache.py"`
 # might use the processes var in order to print statements regarding who
 # is being killed
