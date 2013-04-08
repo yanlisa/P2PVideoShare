0ecc8a1147f887d7296600ace4879b0467b0a35a
Lisa-EC2 <lisasyan@gmail.com>
2013-04-08 00:00:23 +0000

    Added advanced killing to kill scripts.

--- kill_server.sh
+++ kill_server.sh
@@ -1,13 +1,4 @@
-pids=`ps -af | grep -i "python ../server.py" | awk '{print $2}'`
-#processes=`ps -C python -o pid,cmd | grep "python cache.py"`
-# might use the processes var in order to print statements regarding who
-# is being killed
-for pid in $pids
-do
-    kill $pid
-done
-
-pids=`ps -af | grep -i "python ../server_for_cache.py" | awk '{print $2}'`
+pids=`ps -Aaf | grep -i "python ../server.py" | awk '{print $2}'`
 #processes=`ps -C python -o pid,cmd | grep "python cache.py"`
 # might use the processes var in order to print statements regarding who
 # is being killed
