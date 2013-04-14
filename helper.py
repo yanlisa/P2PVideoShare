f96c942e49d667729e4eb5da59f628714ec81bf1
EC2 Default User <ec2-user@ip-10-76-209-53.ec2.internal>
2013-04-14 22:27:51 +0000

    cache_1

--- helper.py
+++ helper.py
@@ -8,6 +8,8 @@ DEBUGGING_MSG = True
 (temp, temp_str1) = commands.getstatusoutput('~/ftp-git/tracker_ip.sh')
 temp_str2 = temp_str1.replace('.','-').split('-')
 tracker_address = 'http://' + '.'.join(temp_str2[1:5]) + ':8081/req/'
+print temp_str2
+print tracker_address
 
 def load_tracker_address():
     return tracker_address
