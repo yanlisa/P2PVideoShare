c5e28417a7db3ae59215a870eb91a68d50b64e10
Kangwook Lee <kw1jjang@gmail.com>
2013-05-17 18:02:34 -0700

    Update_info_loop

--- user.py
+++ user.py
@@ -91,6 +91,64 @@ class P2PUser():
             movie_name = row[0]
             self.movies_LUT[movie_name] = (int(row[1]), int(row[2]), int(row[3]), int(row[4]), int(row[5]), int(row[6]))
 
+    def update_info(self, video_name, code_param_n, code_param_k):
+        filename = 'file-' + video_name + '.' + str(1)
+        folder_name = 'video-' + video_name + '/' + video_name + '.' + str(1) + '.dir/'
+        inst_CNKS = 'CNKS ' + filename
+        inst_UPDG = 'UPDG '
+
+        # self.clients : list of connected guys
+        ct_loop = 99
+        while True:
+            ct_loop += 1
+            if ct_loop = 100:
+                # Copy self.clients to client_copy
+                clients_copy = []
+                for each in self.clients:
+                    each_ip = each.address
+                    clients_copy.append(ThreadClient(each_ip, self.packet_size, 1))
+                ct_loop = 0
+
+            available_chunks = [0]*len(clients_copy) # available_chunks[i] = cache i's availble chunks
+            rates = [0]*len(clients_copy) # rates[i] = cache i's offered rate
+            union_chunks = [] # union of all available indices
+            for i in range(len(clients_copy)):
+                client = clients_copy[i]
+                client.put_instruction(inst_CNKS)
+                return_str = client.get_response().split('&')
+                if return_str[0] == '':
+                    available_chunks[i] = []
+                else:
+                    available_chunks[i] = map(str, return_str[0].split('%'))
+                    for j in range(len(available_chunks[i])):
+                        available_chunks[i][j] = available_chunks[i][j].zfill(2)
+                rates[i] = int(return_str[1])
+                union_chunks = list( set(union_chunks) | set(available_chunks[i]) )
+
+            ## index assignment here
+            # Assign chunks to cache using cache_chunks_to_request.
+            print '[user.py] Update_info_loop : Rates ', rates
+            print '[user.py] Update_info_loop : Available chunks', available_chunks
+
+            assigned_chunks = cache_chunks_to_request(available_chunks, rates, code_param_n, code_param_k)
+
+            effective_rates = [0]*len(rates)
+            for i in range(len(rates)):
+                effective_rates[i] = len(assigned_chunks[i])
+
+            chosen_chunks = [j for i in assigned_chunks for j in i]
+
+            flag_deficit = int(sum(effective_rates) < code_param_k) # True if user needs more rate from caches
+
+            # request assigned chunks
+            for i in range(len(clients_copy)):
+                client = clients_copy[i]
+                print "[user.py] Update_info_loop : [Client " + str(i) + "] flag_deficit: ", flag_deficit, \
+                    ", Assigned chunks: ", assigned_chunks[i]
+                client.put_instruction(inst_UPDG + str(flag_deficit))
+
+            sleep(0.1)
+
     def download(self, video_name, start_frame):
         print '[user.py] P2Puser starts downloading'
         connected_caches = []
@@ -136,6 +194,8 @@ class P2PUser():
         # the queue.
         base_file = open('video-' + video_name + '/' + base_file_name, 'ab')
         base_file_full_path = os.path.abspath('video-' + video_name + '/' + base_file_name)
+
+        thread.start_new_thread( update_info, (video_name, code_param_n, code_param_k) )
 
         for frame_number in xrange(start_frame, num_frames + 1):
             sys.stdout.flush()
