bea714843c2ec23db2d4c362f0b8faa843a39462
Kangwook Lee <kw1jjang@gmail.com>
2013-06-18 05:54:08 -0700

    debug

--- user.py
+++ user.py
@@ -112,14 +112,14 @@ class P2PUser():
         choke_ct = 0
 
         for i in range(self.num_of_caches):
-            each_client = ThreadClient(self, user, cache_ip_addr[i], self.packet_size, i)
+            each_client = ThreadClient(self, cache_ip_addr[i], self.packet_size, i)
             each_client.put_instruction('ID %s' % self.user_name)
             self.clients.append(each_client)
             connected_caches.append(each_client)
             print '[user.py] ', i, 'th connection is CONNECTED : ' , cache_ip_addr[i]
 
         for i in range(self.num_of_caches, len(cache_ip_addr)):
-            each_client = ThreadClient(self, user, cache_ip_addr[i], self.packet_size, i)
+            each_client = ThreadClient(self, cache_ip_addr[i], self.packet_size, i)
             each_client.put_instruction('ID %s' % self.user_name)
             not_connected_caches.append(each_client)
             print '[user.py] ', i, 'th connection is RESERVED: ' , cache_ip_addr[i]
