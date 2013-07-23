fd0293a97b35a5f9068589dba1a9c4c8bd7a76d4
Kangwook Lee <kw1jjang@gmail.com>
2013-07-23 05:18:21 -0700

    new demo

--- demo.py
+++ demo.py
@@ -34,6 +34,29 @@ import vlc
 import os
 import user
 
+class MainFrame(wx.Frame):
+    """The main window has to deal with events.
+    """
+    def __init__(self, title):
+        wx.Frame.__init__(self, None, -1, title,
+                          pos=wx.DefaultPosition, size=(800, 800))
+
+        # Menu Bar
+        #   File Menu
+        self.mainpanel = wx.Panel(self, -1)
+        self.mainpanel.SetBackgroundColour(wx.WHITE)
+
+        # Put everything togheter
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.mainpanel, 1, flag=wx.EXPAND)
+        self.SetSizer(sizer)
+        self.SetMinSize((350, 300))
+
+    def OnExit(self, evt):
+        """Closes the window.
+        """
+        self.Close()
+
 class Player(wx.Frame):
     """The main window has to deal with events.
     """
@@ -218,7 +241,7 @@ if __name__ == "__main__":
     # Create a wx.App(), which handles the windowing system event loop
     app = wx.PySimpleApp()
     # Create the window containing our small media player
-    player = Player("Simple PyVLC Player")
+    player = MainFrame("MASCOTS 2013 Demo")
     # show the player window centred and run the application
     player.Centre()
     player.Show()
