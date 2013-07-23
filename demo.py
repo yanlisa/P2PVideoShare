982e0687128402d3f96a819ee63ae0a19c8a469e
Kangwook Lee <kw1jjang@gmail.com>
2013-07-23 05:20:58 -0700

    change

--- demo.py
+++ demo.py
@@ -34,28 +34,7 @@ import vlc
 import os
 import user
 
-class MainFrame(wx.Frame):
-    """The main window has to deal with events.
-    """
-    def __init__(self, title):
-        wx.Frame.__init__(self, None, -1, title,
-                          pos=wx.DefaultPosition, size=(800, 800))
-
-        # Menu Bar
-        #   File Menu
-        self.mainpanel = wx.Panel(self, -1)
-        self.mainpanel.SetBackgroundColour(wx.WHITE)
-
-        # Put everything togheter
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(self.mainpanel, 1, flag=wx.EXPAND)
-        self.SetSizer(sizer)
-        self.SetMinSize((350, 300))
-
-    def OnExit(self, evt):
-        """Closes the window.
-        """
-        self.Close()
+from gui.mainframe import MainFrame
 
 class Player(wx.Frame):
     """The main window has to deal with events.
